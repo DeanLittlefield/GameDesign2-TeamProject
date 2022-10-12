b93717b54504fff35caf1f95d3ad9f624ba0680b
sthompson717 <stephenchristopherthompson@gmail.com>
2022-10-12 11:01:09 -0500

    Vertical and horizontal player controls, camera follows player

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -10,9 +10,17 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
+        transform.Translate(Vector3.right*Time.deltaTime*turnSpeed*horizontalInput);
     }
 }
